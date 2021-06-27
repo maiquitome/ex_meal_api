ae9fa7ea74512167f86839b6431571f2e476b141
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-06-27 18:22:34 -0300

    :sparkles: add create action

--- lib/ex_meal_web/controllers/meal_controller.ex
+++ lib/ex_meal_web/controllers/meal_controller.ex
@@ -1,3 +1,15 @@
 defmodule ExMealWeb.MealController do
   use ExMealWeb, :controller
+
+  alias ExMeal.Meals.{Create, Meal}
+
+  action_fallback ExMealWeb.FallbackController
+
+  def create(conn, params) do
+    with {:ok, %Meal{} = meal} <- Create.call(params) do
+      conn
+      |> put_status(:created)
+      |> render("create.json", meal: meal)
+    end
+  end
 end
