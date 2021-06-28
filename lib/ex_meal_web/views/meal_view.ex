55ce72e68e383601337c539551f127f2a2146a2e
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-06-28 09:26:39 -0300

    :sparkles: render create.json

--- lib/ex_meal_web/views/meal_view.ex
+++ lib/ex_meal_web/views/meal_view.ex
@@ -1,3 +1,11 @@
 defmodule ExMealWeb.MealView do
   use ExMealWeb, :view
+
+  alias ExMeal.Meals.Meal
+
+  def render("create.json", %{meal: %Meal{} = meal}) do
+    %{
+      meal: meal
+    }
+  end
 end
