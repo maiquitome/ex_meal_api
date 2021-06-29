f5513922390477bcff5418df90da8fe86677a8c6
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-06-29 13:07:06 -0300

    :sparkles: add create_meal facade

--- lib/ex_meal.ex
+++ lib/ex_meal.ex
@@ -6,4 +6,37 @@ defmodule ExMeal do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+  alias ExMeal.Error
+  alias ExMeal.Meals.Meal
+  alias ExMeal.Meals.Create, as: CreateMeal
+
+  @typedoc """
+  Meal params.
+  """
+  @type meal_params :: %{
+          calories: String.t(),
+          date: Calendar.naive_datetime(),
+          description: String.t()
+        }
+
+  @doc """
+  Inserts a meal into the database.
+
+  ## Examples
+
+        iex> params = %{calories: "100 kcal", date: "2016-04-16 13:30:15", description: "1 Ovo"}
+
+        iex> ExMeal.create_meal(params)
+        {:ok, %Meal{}}
+
+        iex> ExMeal.create_meal(%{})
+        %ExMeal.Error{result: %Ecto.Changeset{}, status: :bad_request}
+
+  """
+  @spec create_meal(meal_params()) ::
+          {:ok, Meal.t()}
+          | {:error, %Error{result: Ecto.Changeset.t(), status: :bad_request}}
+  defdelegate create_meal(meal_params),
+    to: CreateMeal,
+    as: :call
 end
--- lib/ex_meal_web/controllers/meal_controller.ex
+++ lib/ex_meal_web/controllers/meal_controller.ex
@@ -1,12 +1,12 @@
 defmodule ExMealWeb.MealController do
   use ExMealWeb, :controller
 
-  alias ExMeal.Meals.{Create, Meal}
+  alias ExMeal.Meals.Meal
 
   action_fallback ExMealWeb.FallbackController
 
   def create(conn, params) do
-    with {:ok, %Meal{} = meal} <- Create.call(params) do
+    with {:ok, %Meal{} = meal} <- ExMeal.create_meal(params) do
       conn
       |> put_status(:created)
       |> render("create.json", meal: meal)
