5d765344c68f051744a2559754f199b153380e8d
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-06-30 16:22:38 -0300

    :white_check_mark: add get tests

--- test/ex_meal_web/controllers/meal_controller_test.exs
+++ test/ex_meal_web/controllers/meal_controller_test.exs
@@ -47,4 +47,36 @@ defmodule ExMealWeb.MealControllerTest do
     end
   end
 
+  describe "show/2" do
+    test "when there is an user with the given id, returns the meal from the database.", %{
+      conn: conn
+    } do
+      %Meal{id: id} = insert(:meal)
+
+      response =
+        conn
+        |> get(Routes.meal_path(conn, :show, id))
+        |> json_response(:ok)
+
+      assert %{
+               "meal" => %{
+                 "calories" => "100 kcal",
+                 "date" => "2016-04-16T13:30:15",
+                 "description" => "1 Ovo",
+                 "id" => "84dc958f-4e96-4b1f-b164-ea0ba4a2a0e8"
+               }
+             } = response
+    end
+
+    test "when there is no an user with the given id, returns an error.", %{conn: conn} do
+      id = "1e459e18-5847-4832-8aeb-4c29a869b7be"
+
+      response =
+        conn
+        |> get(Routes.meal_path(conn, :show, id))
+        |> json_response(:not_found)
+
+      assert %{"message" => "Meal not found!"} = response
+    end
+  end
 end
--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,11 +1,22 @@
 defmodule ExMeal.Factory do
   use ExMachina.Ecto, repo: ExMeal.Repo
 
+  alias ExMeal.Meals.Meal
+
   def meal_params_factory do
     %{
       calories: "100 kcal",
       date: "2016-04-16 13:30:15",
       description: "1 Ovo"
+    }
+  end
+
+  def meal_factory do
+    %Meal{
+      calories: "100 kcal",
+      date: "2016-04-16T13:30:15",
+      description: "1 Ovo",
+      id: "84dc958f-4e96-4b1f-b164-ea0ba4a2a0e8"
     }
   end
 end
